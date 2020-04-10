7c49170696986acc1af5b909375467124fd85001
mukesh <mukeshbhardwaj101@gmail.com>
2020-04-10 16:23:13 +0530

    commited with new class Hai.class

--- Test_Git/src/com/test/Test.java
+++ Test_Git/src/com/test/Test.java
@@ -1,9 +1,16 @@
 package com.test;
 
+class Hai{
+	static void m1(){
+		System.out.println("printing hi msg");
+	}
+}
+
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to java world!");
+		Hai.m1();
 	}
 
 }
